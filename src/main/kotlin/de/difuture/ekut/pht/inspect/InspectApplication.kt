15503ad246ac15c712b98486a99bb8b40c0ce78f
Lukas Zimmermann <luk.zim91@gmail.com>
2018-10-23 16:14:47 +0200

    Init project

---
+++ src/main/kotlin/de/difuture/ekut/pht/inspect/InspectApplication.kt
@@ -0,0 +1,11 @@
+package de.difuture.ekut.pht.inspect
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class InspectApplication
+
+fun main(args: Array<String>) {
+    runApplication<InspectApplication>(*args)
+}
